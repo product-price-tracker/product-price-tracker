2551fd6b2c6e0a520e3cb0bed6115a0d32ded0b6
mkwatra427 <mitrankwatra@gmail.com>
2019-10-08 13:51:30 -0400

    added ratePrice script with rate_price, current_price, and mean_price_history functions

---
+++ Python Scripts/ratePrice.py
@@ -0,0 +1,25 @@
+import keepa
+import pandas
+from getAccessKey import getAccessKey
+from getProductData import get_data_for_product
+
+# for any ASIN, say whether it is under or overpriced
+
+def rate_price(asin):
+
+    df = get_data_for_product(asin)
+    currentPrice = df.at[len(df) - 1, 'NEW']
+    meanHistoryPrice = df.loc[:, 'NEW'].mean()
+    return currentPrice > meanHistoryPrice
+
+def current_price(asin):
+    df = get_data_for_product(asin)
+    return df.at[len(df) - 1, 'NEW']
+
+def mean_history_price(asin):
+    df = get_data_for_product(asin)
+    return df.loc[:, 'NEW'].mean()
+
+if __name__ == "__main__":
+    print(rate_price('B0775451TT'))
+
