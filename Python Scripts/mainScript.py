ed772f970dc336023befc46b2270e42739129247
LRadics <36389527+LRadics@users.noreply.github.com>
2019-10-28 22:02:02 -0400

    better idea for mainScript
    thought of a better way to write what I'd written, so started to fix it, will do more later (still a bit sick)

--- Python Scripts/mainScript.py
+++ Python Scripts/mainScript.py
@@ -1,4 +1,4 @@
-#python mainScript.py <command> <ASIN> [category] [numProducts] [-h]
+#python mainScript.py <command> <ASIN or "category"> <time period> [optional detail] [-h]
 
 import argparse #command-line parsing module
 
@@ -7,14 +7,18 @@ from getProductsForCategory import getProductDataForCategory
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("command", choices=['ratePrice', 'catProducts'], help="command to be run - required")
+parser.add_argument("command", choices=['historic', 'data', 'predict', 'findFutureMin'], help="command to be run - required")
 parser.add_argument("ASIN", help="the ASIN of interest")
-parser.add_argument("category", default=0, nargs='?', help="the root category of products")
-parser.add_argument("numProducts", default=0, nargs='?', help="number of products to a page")
+parser.add_argument("time", choices=['day', 'week', 'month', 'year'], help="the root category of products")
+parser.add_argument("details", default=0, nargs='?', help="number of products to a page")
 
 args = parser.parse_args()
 
-if (args.command == "ratePrice"):
+bool asinIsCategory = False
+# Lee to do - add check so that user can input 'category' instead of an ASIN and receive a prompt to enter the category
+# (from there they can receive information about the items in that category)
+
+if (args.command == "historic"):
     print(rate_price(args.ASIN))
-elif(args.command == "catProducts"):
+elif(args.command == "historic"):  # check that asinIsCategory is equal to true
     getProductDataForCategory(args.category, args.numProducts)
