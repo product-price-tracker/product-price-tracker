79017182bfe4f1d343d0751e2e9fecdff727d037
LRadics <36389527+LRadics@users.noreply.github.com>
2019-10-31 14:09:55 -0400

    more updates to mainScript
    still not quite done but almost there

--- Python Scripts/mainScript.py
+++ Python Scripts/mainScript.py
@@ -1,24 +1,40 @@
-#python mainScript.py <command> <ASIN or "category"> <time period> [optional detail] [-h]
+#python mainScript.py <command> <ASIN or "category"> <time period> [number of products] [-h]
 
 import argparse #command-line parsing module
 
 from ratePrice import rate_price
 from getProductsForCategory import getProductDataForCategory
+from predictNextPrice import predict_upcoming_prices
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("command", choices=['historic', 'data', 'predict', 'findFutureMin'], help="command to be run - required")
+parser.add_argument("command", choices=['historic', 'predict', 'predictSequence', 'findFutureMin'], help="command to be run - required")
 parser.add_argument("ASIN", help="the ASIN of interest")
 parser.add_argument("time", choices=['day', 'week', 'month', 'year'], help="the root category of products")
-parser.add_argument("details", default=0, nargs='?', help="number of products to a page")
+parser.add_argument("numProducts", default=50, nargs='?', help="number of products")
 
 args = parser.parse_args()
 
-asinIsCategory = False
-# Lee to do - add check so that user can input 'category' instead of an ASIN and receive a prompt to enter the category
-# (from there they can receive information about the items in that category)
+numDays = 1
+if (args.time == 'day'):
+    numDays = 1
+elif (args.time == 'week'):
+    numDays = 7
+elif (args.time == 'month'):
+    numDays = 30 #assume 30-day month
+elif (args.time == 'year'):
+    numDays = 365
+
+bool asinIsCategory = False
+if (args.ASIN == "category"):
+    cat = input("Enter root category number:")
+    asinIsCategory = True
 
 if (args.command == "historic"):
     print(rate_price(args.ASIN))
-elif(args.command == "historic"):  # check that asinIsCategory is equal to true
-    getProductDataForCategory(args.category, args.numProducts)
+elif(args.command == "historic" and asinIsCategory == True):  
+    getProductDataForCategory(cat, args.numProducts)
+elif(args.command == "predict"):
+    predict_upcoming_prices(numDays, 150, 10, args.ASIN, price='NEW')
+elif(args.command == "predictSequence"):
+elif(args.command == "findFutureMin"):
