17f18acc8ced74078ff10d8bb81ab89511b4d55d
marcusdaly <mrd109@case.edu>
2019-11-19 14:08:34 -0500

    Start creating data types to store price data in frontend

---
+++ Frontend/ppt-frontend/src/classes/PricePoint.ts
@@ -0,0 +1,5 @@
+export default class PricePoint {
+  new?: number;
+  amazon?: number;
+  used?: number;
+}
--- Frontend/ppt-frontend/src/components/history-page/history-page.ts
+++ Frontend/ppt-frontend/src/components/history-page/history-page.ts
@@ -1,6 +1,8 @@
 import { Component, Prop, Vue, Ref } from 'vue-property-decorator';
 
-import Page from '../page/index.vue'
+import Page from '../page/index.vue';
+
+import axios from "axios";
 
 @Component({
   name: 'history-page',
@@ -9,4 +11,15 @@ import Page from '../page/index.vue'
 })
 export default class HistoryPage extends Page {
   asin: string = "";
+  base: string = "http://localhost:5000";
+  priceList: PriceList = {};
+
+  onHistory() {
+    // TODO Make HTTP Request to rate price.
+    axios.get(this.base + '/price-data', {
+      params: {asin: this.asin}
+    }).then((response) => {
+      this.priceRating = response.data
+    })
+  }
 }
